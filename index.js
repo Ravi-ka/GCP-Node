1d4f0d51bfe9a1e6f93b03d22b5f653e4b9c3df2
Ravisankar Ka <ravisankar.ka@accenture.com>
2026-02-07 14:56:30 +0530

    test commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; // add this is in environment variable in cloud run
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
